d759778d0342f5e845c0ceb8eeb0824f5bb67ba4
Gabriela Maureira <gmaureirapalma@gmail.com>
2023-07-17 13:25:02 +0200

    Daisy ui instaled

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,10 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello daaaaarlings
+          Hello daaaaarlings <button
+          class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+          Button
+        </button>
         </p>
       </div>
       <p className="read-the-docs">
